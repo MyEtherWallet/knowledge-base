e73b55ecabe7f58c9508b841804746188666199e
David Hong <hsurf22@gmail.com>
2019-04-10 18:37:09 -0700

    Mobile update

---
+++ themes/mew5/source/js/initialization.js
@@ -0,0 +1,19 @@
+/*
+
+(function($){
+
+  var width = $( window ).width();
+  //console.log(width);
+  
+  if(width < 600) {
+    // Mobile
+    $('.desktop-content').addClass('hidden');
+  }
+  else {
+    // Desktop
+    $('.mobile-content').addClass('hidden');
+  }
+
+})(jQuery);
+
+*/
