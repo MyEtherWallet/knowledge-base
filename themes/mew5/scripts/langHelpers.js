6335ecae0c78abc2b03deb7b204ef92c3d66a87e
SteveM <SteveM@myEtherWallet.com>
2019-05-30 20:32:37 -0700

    check point

--- themes/mew5/scripts/langHelpers.js
+++ themes/mew5/scripts/langHelpers.js
@@ -1,12 +1,18 @@
 
 
 
-hexo.extend.helper.register('lang_url', (base) => {
+hexo.extend.helper.register('strip_lang_prefix', (path, langs = []) => {
   // const { config } = hexo;
   // const lang =
   // console.log(hexo.page); // todo remove dev item
   // const url_for = hexo.extend.helper.get('url_for').bind(hexo);
   // const url = url_for(path).bind(hexo);
   // console.log(path); // todo remove dev item
-  return base;
+  let newPath = path.replace(/^\/default\//, '');
+  langs.forEach(lang => {
+    const regEx = `/${lang}/`
+    newPath = newPath.replace(regEx, '')
+  })
+  return newPath;
+  // return base;
 });
