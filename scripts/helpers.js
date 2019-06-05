34e32ddd5721fe57ceed4d994cbd8a4dc2450468
SteveM <SteveM@myEtherWallet.com>
2019-06-04 18:03:15 -0700

    clean up

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -2,8 +2,6 @@
 
 'use strict';
 
-var pathFn = require('path');
-
 function startsWith(str, start) {
   return str.substring(0, start.length) === start;
 }
@@ -17,37 +15,6 @@ hexo.extend.filter.register('after_post_render', function(data){
     }
   }
   return data;
-});
-
-
-hexo.extend.helper.register('page_nav', function() {
-  var type = this.page.canonical_path.split('/')[0];
-  var sidebar = this.site.data.sidebar[type];
-  var path = pathFn.basename(this.path);
-  var list = {};
-  var prefix = 'sidebar.' + type + '.';
-
-  for (var i in sidebar) {
-    for (var j in sidebar[i]) {
-      list[sidebar[i][j]] = j;
-    }
-  }
-
-  var keys = Object.keys(list);
-  var index = keys.indexOf(path);
-  var result = '';
-
-  if (index > 0) {
-    result += '<a href="' + keys[index - 1] + '" class="article-footer-prev" title="' + this.__(prefix + list[keys[index - 1]]) + '">'
-      + '<i class="fa fa-chevron-left"></i><span>' + this.__('page.prev') + '</span></a>';
-  }
-
-  if (index < keys.length - 1) {
-    result += '<a href="' + keys[index + 1] + '" class="article-footer-next" title="' + this.__(prefix + list[keys[index + 1]]) + '">'
-      + '<span>' + this.__('page.next') + '</span><i class="fa fa-chevron-right"></i></a>';
-  }
-
-  return result;
 });
 
 hexo.extend.helper.register('canonical_url', function(lang) {
@@ -82,10 +49,6 @@ hexo.extend.helper.register('url_for_lang', function(path) {
   }
   if (lang) url = '/' + lang + url;
   return url;
-});
-
-hexo.extend.helper.register('raw_link', function(path) {
-  return 'https://github.com/hexojs/site/edit/master/source/' + path;
 });
 
 
