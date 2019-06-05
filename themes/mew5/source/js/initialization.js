1d389a912f2aeac20a694d9efe90a59c6cc28c1b
SteveM <SteveM@myEtherWallet.com>
2019-06-04 19:06:04 -0700

    fixed missing main language index files

--- themes/mew5/source/js/initialization.js
+++ themes/mew5/source/js/initialization.js
@@ -22,21 +22,10 @@ function selectMobileDesktop () {
 
 (function ($) {
   function changeLang () {
-    console.log(this); // todo remove dev item
-
     var lang = this.value;
-    var canonical = this.dataset.canonical;
-    var modifiedCanonical = canonical.replace(/^\w\w\//, '');
     var path = '/';
     path += lang + '/';
     location.href = path;
-    // if (canonical.includes(noRoute[0]) || canonical.includes(noRoute[1])) {
-    //   location.href = path;
-    // } else {
-    //   location.href = path;
-    // }
-    // // Cookies.set('nf_lang', lang, { expires: 365 });
-    // location.href = path + modifiedCanonical;
   }
 
   $('#lang-select').change(changeLang);
