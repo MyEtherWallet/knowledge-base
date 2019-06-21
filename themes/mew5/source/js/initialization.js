472eb055e3ef19ff04790fa43f4a6e9ff3e6c42f
Steve Mieskoski <Stevemieskoski@gmail.com>
2019-06-20 19:56:40 -0700

    Revert "Translation setup (#19)" (#21)
    This reverts commit f85821afbd6264e9766e859ff20c071c1ca2507e.

--- themes/mew5/source/js/initialization.js
+++ themes/mew5/source/js/initialization.js
@@ -1,12 +1,14 @@
-function selectMobileDesktop () {
-  var width = $(window).width();
 
-  if (width < 600) {
+function selectMobileDesktop() {
+  var width = $( window ).width();
+  
+  if(width < 600) {
     // Mobile
     //$('.desktop-content').addClass('hidden');
     $('.mobile-content').addClass('block');
     $('.desktop-content').remove();
-  } else {
+  }
+  else {
     // Desktop
     $('.desktop-content').addClass('block');
     $('.mobile-content').remove();
@@ -14,54 +16,8 @@ function selectMobileDesktop () {
 }
 
 
-(function ($) {
+(function($){
   selectMobileDesktop();
 
 })(jQuery);
 
-
-(function ($) {
-  function changeLang () {
-    var lang = this.value;
-    var path = '/';
-    path += lang + '/';
-    location.href = path;
-  }
-
-  $('#lang-select').change(changeLang);
-  $('#mobile-lang-select').change(changeLang);
-
-})(jQuery);
-
-(function ($) {
-
-  // if($('#lang-select').hasClass('hidden') && !$('#lang-select-label').hasClass('hidden')){
-  //   $('#lang-select').removeClass('hidden');
-  //   $('#lang-select-label').addClass('hidden');
-  // } else {
-  //   $('#lang-select').addClass('hidden');
-  //   $('#lang-select-label').removeClass('hidden');
-  // }
-
-  $('#lang-select-label').click(function(){
-    if($('#lang-select').hasClass('hidden') && !$('#lang-select-label').hasClass('hidden')){
-      $('#lang-select').removeClass('hidden');
-      $('#lang-select-label').addClass('hidden');
-    } else {
-      $('#lang-select').addClass('hidden');
-      $('#lang-select-label').removeClass('hidden');
-    }
-
-
-  });
-  $('#lang-select').change(function(){
-    if(!$('#lang-select').hasClass('hidden') && $('#lang-select-label').hasClass('hidden')){
-      $('#lang-select').addClass('hidden');
-      $('#lang-select-label').removeClass('hidden');
-    } else {
-      $('#lang-select').removeClass('hidden');
-      $('#lang-select-label').addClass('hidden');
-    }
-  });
-
-})(jQuery);
