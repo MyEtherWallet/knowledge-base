2c19f13ee121bc319875607b0a1861a4c0081c27
SteveM <SteveM@myEtherWallet.com>
2019-06-24 15:28:42 -0700

    fix missing code

--- themes/mew5/scripts/lang_select.js
+++ themes/mew5/scripts/lang_select.js
@@ -3,8 +3,15 @@
 
   function changeLang(){
     var lang = this.value;
-    console.log(lang); // todo remove dev item
-    if(lang === 'none') return;
     var canonical = this.dataset.canonical;
     if (lang === 'en') lang = '';
     if (lang) lang += '/';
+
+    location.href = '/' + lang + canonical;
+  }
+if(typeof document !== 'undefined'){
+  document.getElementById('lang-select').addEventListener('change', changeLang);
+  document.getElementById('mobile-lang-select').addEventListener('change', changeLang);
+}
+
+})();
