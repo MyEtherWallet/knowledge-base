903396381674b9f9911bc18dbf569f8602b171d5
SteveM <SteveM@myEtherWallet.com>
2019-09-20 12:16:18 -0700

    add link from faq to main info page

--- themes/mew5/source/js/mew/accordion.js
+++ themes/mew5/source/js/mew/accordion.js
@@ -3,6 +3,10 @@
   // Add ".open" to clicked category
   $('.accordion-header').on('click', function(){
     $(this).parent().toggleClass('open');
+    console.log($(this)); // todo remove dev item
+    console.log($(this).children()); // todo remove dev item
+    console.log($($(this).children()[0])); // todo remove dev item
+    $('.goto-page-link').toggleClass('hidden')
   });
   
 })(jQuery);
