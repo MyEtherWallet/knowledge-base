d2b207c9413a25bd971adf050da840b2ef23cef4
Jessica <jessicap@myetherwallet.com>
2019-10-21 13:15:58 -0700

    Cleaning

--- themes/mew5/source/js/mew/header-post-search.js
+++ themes/mew5/source/js/mew/header-post-search.js
@@ -1,20 +1,21 @@
+
 function occurrences(string, subString, allowOverlapping) {
   string += "";
   subString += "";
   if (subString.length <= 0) return (string.length + 1);
-      var n = 0,
-        pos = 0,
-        step = allowOverlapping ? 1 : subString.length;
 
-    while (true) {
-        pos = string.indexOf(subString, pos);
-        if (pos >= 0) {
-            ++n;
-            pos += step;
-        } else break;
-    }
+  var n = 0,
+      pos = 0,
+      step = allowOverlapping ? 1 : subString.length;
 
-    return n;
+  while (true) {
+      pos = string.indexOf(subString, pos);
+      if (pos >= 0) {
+          ++n;
+          pos += step;
+      } else break;
+  }
+  return n;
 }
 
 function searchPosts(_this) {
