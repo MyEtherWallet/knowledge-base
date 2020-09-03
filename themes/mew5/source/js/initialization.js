4f2a6f5303c40aee48016a0fab4a9c08830f5576
Mihail <mpolev364@gmail.com>
2020-09-03 22:44:20 +0700

    create app banner (#51)
    * create app banner
    
    * update link

--- themes/mew5/source/js/initialization.js
+++ themes/mew5/source/js/initialization.js
@@ -68,3 +68,55 @@ let leaveTimer = null;
 
 
 })(jQuery);
+
+(function ($) {
+  var closeBanner = getCookie('close-banner');
+
+  if(closeBanner == '1') {
+    $('.js-app-banner').remove();
+  } else {
+    $('.js-app-banner').addClass('visible');
+  }
+
+  $('.js-close-app-banner').click(function(){
+    setCookie('close-banner', '1', {});
+    $('.js-app-banner').remove();
+  });
+
+  $('.js-get-app').click(function(){
+    $('.js-get-app').next().addClass('visible');
+    $('.js-get-app').remove();
+  });
+  
+})(jQuery);
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+
+  options = {
+    path: '/',
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
